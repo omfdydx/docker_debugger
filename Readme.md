c060529c7aea9ff75cfe59af5dd9c1726497bda1
Bh0kal <133207407+omfdydx@users.noreply.github.com>
2024-06-21 18:11:17 +0530

    v0.1

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 Lightweight Docker utility for curl, wget, ping, screen, psql, mysql, et al 
 
 Packed with a simple api server to keep it even if there are some scripts up and running in screen or bash
+
+WIP - will keep including tools further needed for debugging Kubernetes / Docker based deployments
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 5050;
 // This server will respond to all requests (regardless of path) with "hello world"
 const server = http.createServer((request, response) => {
+    response.setHeader('ngrok-skip-browser-warning', true);
     response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(JSON.stringify({
         data: 'Hello World!',
